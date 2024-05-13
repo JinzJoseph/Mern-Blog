fbc2b19e3f04a3a199b84ab0cc495911f82396c0
jinzjoseph <jinsjosephkunnummel000@gmail.com>
2024-05-13 11:49:14 +0530

    initialized

---
+++ Client/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+     <h1 className='text-red-600 text-5xl'>hallo</h1>
+    </>
+  )
+}
+
+export default App
